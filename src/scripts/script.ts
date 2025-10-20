628bc44476e3f121eadb171f6bbd3e1d38429ee6
Vladislav <pandaisback62@mail.ru>
2025-10-20 20:46:05 +0300

    remove yt player, add video

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,28 +1,15 @@
-/// <reference types="@types/youtube" />
-
-let player: YT.Player;
-let lastSelectedIndex = -1;
-
-(window as any).onYouTubeIframeAPIReady = function () {
-    player = new YT.Player('player', {
-        height: '100%',
-        width: '100%',
-        videoId: randomClip(LINK_VIDEO),
-        playerVars: {
-            'autoplay': 1,
-            'controls': 0,
-        },
-    });
-}
+let currentVideoIndex = 0;
+let lastVideoIndex = 0;
 
 const playBtn = document.getElementById('playBtn') as HTMLButtonElement;
 const moreContainer = document.getElementById('moreContainer') as HTMLDivElement
+const player = document.getElementById('player') as HTMLVideoElement
 
 const LINK_VIDEO =
 [
- 'nRbuLixWH1E',
- '0bd3Zgf964k',
- 'rdPfJx64CO0',
+ './video/1.mp4',
+ './video/2.mp4',
+ './video/3.mp4',
 ];
 
 function createPositionForMore() {
@@ -30,7 +17,7 @@ function createPositionForMore() {
     const topValue = Math.random() * (60 - 10) + 10;
     const leftValue = Math.random() * (80 - 10) + 10;
     const transformValue = Math.random() * (-50 - 10) + 10;
-
+    const count = moreContainer.querySelectorAll('p').length;
 
     newP.className = 'more';
     newP.textContent = 'more?';
@@ -39,22 +26,36 @@ function createPositionForMore() {
     newP.style.transform = `rotate(${transformValue}deg)`
 
 
+    switch (true) {
+        case (count > 9):
+            newP.style.fontSize = '11rem';
+            break;
+        case (count > 6):
+            newP.style.fontSize = '7rem';
+            break;
+        case (count > 3):
+            newP.style.fontSize = '5rem';
+            break;
+        default:
+            break;
+    }
+
     moreContainer.appendChild(newP);
 }
 
-const randomClip = (clipList: string[]) => {
-    let randomIndex;
+function getRandomIndexExceptPrev(prev: number, max: number) {
+    let randomIndex: number;
     do {
-        randomIndex = Math.floor(Math.random() * clipList.length);
-    } while (randomIndex === lastSelectedIndex && clipList.length > 1);
-
-    lastSelectedIndex = randomIndex;
-    return clipList[randomIndex];
+        randomIndex = Math.floor(Math.random() * max);
+    } while (randomIndex === prev && max > 1);
+    return randomIndex;
 }
 
 playBtn.addEventListener('click', () => {
-    const videoId = randomClip(LINK_VIDEO)
-    if (videoId != undefined)
-    player.loadVideoById(videoId)
+    const nextIndex = getRandomIndexExceptPrev(lastVideoIndex, LINK_VIDEO.length);
+    lastVideoIndex = nextIndex;
+    player.src = LINK_VIDEO[nextIndex] || '';
+    player.load();
+    player.play();
     createPositionForMore()
 })
